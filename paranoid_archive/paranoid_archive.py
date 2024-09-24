d83119dfd0b38768ee7e46eb7bd9e55679d4ef55
Yucheng Huang <yuh032@ucsd.edu>
2024-09-24 15:44:44 -0700

    completed basic function of PA

--- paranoid_archive/paranoid_archive.py
+++ paranoid_archive/paranoid_archive.py
@@ -2,7 +2,10 @@ import os
 import sys
 import bcrypt
 import zipfile
+import io
 
+
+ZIP_SIGNATURE = b'\x50\x4b\x03\x04'
 def create_archive(correct_pwd, trap_pwd, correct_file, trap_file, archive_name):
     correct_pwd_hash = bcrypt.hashpw(correct_pwd.encode('utf-8'), bcrypt.gensalt())
     trap_pwd_hash = bcrypt.hashpw(trap_pwd.encode('utf-8'), bcrypt.gensalt())
@@ -44,10 +47,42 @@ def extract_archive(archive_name, password):
         second_hash_block = f.read(60)
         print(first_hash_block)
         print(second_hash_block)
+        data = f.read()
+        first_zip, second_zip = separate_files(data)
+
         if bcrypt.checkpw(password.encode('utf-8'), first_hash_block):
+            extract_zipfile(first_zip, password)
             print("correct password")
+        elif bcrypt.checkpw(password.encode('utf-8'), second_hash_block):
+            extract_zipfile(second_zip, password)
+            print("trap password entered")
         else:
-            print("incorrect password")
+            print("wrong password")
+
+
+
+
+def separate_files(data):
+    first_zip_start = data.find(ZIP_SIGNATURE)
+    if first_zip_start == -1:
+        raise ValueError("First ZIP signature not found")
+    second_zip_start = data.find(ZIP_SIGNATURE, first_zip_start + 1)
+    if second_zip_start == -1:
+        raise ValueError("Second ZIP signature not found")
+    first_zip = data[first_zip_start:second_zip_start]
+    second_zip = data[second_zip_start:]
+    return first_zip, second_zip
+
+def extract_zipfile(data, password):
+    output_dir = os.getcwd()
+    with io.BytesIO(data) as file_like_object:
+
+        with zipfile.ZipFile(file_like_object, 'r') as zipf:
+            for file_info in zipf.infolist():
+                try:
+                    zipf.extract(file_info, path=output_dir, pwd=bytes(password, 'utf-8'))
+                except RuntimeError as e:
+                    print(f"Failed to extract {file_info.filename}: {e}")
 
 if __name__ == "__main__":
     print("working...")
